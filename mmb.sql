7889a60d8374bfc19516f3f580628b5b600b39f1
Harsh Kumar Shekhar <84192089+shekhar610@users.noreply.github.com>
2021-09-23 02:32:34 +0530

    Add files via upload

---
+++ mmb.sql
@@ -0,0 +1,14 @@
+
+
+
+show databases; 
+use movie; 
+create table movies ( movie_name varchar (20), actor varchar ( 20) , actoress varchar (20 ), yearofrelease  date , director_name  varchar (20));
+select * from movies ; 
+insert into movies values  ('SHRESHA',  'Siddharth', 'kaira' , '2021-05-21' , 'yash' );
+select * from movies ; 
+insert into movies values  ('BEllBOTTOM',  'AKSHAY', 'NAGRIS' , '2021-08-21' , 'IMTIYAZ' );
+insert into movies values  ('RAZI',  'VARUN', 'ALIYA ' , '2020-12-12' , 'YASHRAJ' );
+select * from movies ; 
+
+
